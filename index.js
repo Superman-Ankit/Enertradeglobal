7cf0871ead533c51b60aa26c237fe473fe4b2894
Superman-Ankit <sehrawatankit01@gmail.com>
2025-07-10 03:16:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to EnerTrade Global</h1>
-      <p>Your trusted India–Russia Energy Trading Partner</p>
-    </div>
-  );
-}
+
